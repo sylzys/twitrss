c3cc6a064543cd77dbdacc12b2b29077b4ac0493
Syl Zys <sylzys@gmail.com>
2012-05-06 12:35:16 +0200

    avant onglets

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,21 @@
 (function($) {
-var counter = 1;
- $('#add').click(function(event) {
-  if (++counter < 5){
-    console.log("adding a div: " + counter);
+  var counter = 1;
+  $('#add').click(function(event) {
+    if (++counter < 5){
+      console.log("adding a div: " + counter);
     // enlarge container 10% more
     $('#container').width(50+(counter * 10)+"%");
     //appendinga new div
     var newdiv = $('#template').clone().appendTo('#container');
-    newdiv.append("div2");
+    newdiv.append("div"+counter);
     newdiv.show();
     //modify each content div
     $('#container').children('div:not(:first)').each(function(){
-        $(this).removeClass('div1', 'div2', 'div3', 'div4').addClass("div"+counter);
-        
-      });
+      $(this).removeClass('div1', 'div2', 'div3', 'div4').addClass("div"+counter);
+
+    });
     $.ajax({
-        url:  'http://search.twitter.com/search.json?q=twitterapi',
+      url:  'http://search.twitter.com/search.json?q=twitterapi',
         //data: 'twitterapi',
         type:       'GET',
         dataType: "jsonp",
@@ -24,20 +24,38 @@ var counter = 1;
           console.log(data);
         },
         error : function(data){
-            console.log(data);
+          console.log(data);
         }
-    });
+      });
   }
   else 
     console.log("too wide !");
 });
+
+
   // MAIN
   $(function() {
     $('#container').width("50%");
-    
     $('#template').clone().appendTo('#container');
     $('#template').hide();
-  });
+//submit button
+$('.search').click(function(event) {  
+  alert("damnit");
+  console.log("searching for: " + $(this).text());      
+});
+ //END submit button
+
+    //slide panel
+    var sipPos = 0;
+    $("#paneltab").click(function(e) {
+      e.preventDefault();
+      $("#panel").animate({ left: sipPos }, 800, 'linear', function() {
+        if(sipPos == 0) { sipPos = -100; }
+        else { sipPos = 0; }
+      });
+  });//END slide panel
+
+  });//END main
 })(jQuery);
 
 

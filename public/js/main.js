e3b49329df9cfd75e25a4ca2242ab239db035107
Syl Zys <sylzys@gmail.com>
2012-05-09 23:42:25 +0200

    prob div slide

--- public/js/main.js
+++ public/js/main.js
@@ -1,75 +1,79 @@
 (function($) {
+  //VARIABLES
   var counter = 1;
   var css_class = 1;
   var delay = 0;
-  $('#add').on('click', function() {
+//
+
+$('#add').on('click', function() {
     //after 4 divs : make tabs
     if (++counter >= 5){
       var w = $('#container').width()/4 + 15;
       console.log(w);
       console.log("too wide !");
 
-   }
-   (counter < 5) ? css_class = counter : css_class = "tab";
-   (counter < 5) ? $('#container').width(50+(counter * 10)+"%") : $('#container').width("90%");
-  console.log("adding a div: delay" + delay);
-   
+    }
+    (counter < 5) ? css_class = counter : css_class = "tab";
+    (counter < 5) ? $('#container').width(50+(counter * 10)+"%") : $('#container').width("90%");
+    console.log("adding a div: delay" + delay);
+
 // var newdiv = $('#template').clone().appendTo('#container');
 // newdiv.attr("id", counter);
 // newdiv.show();
 
 // setTimeout(function() {
-var newdiv = $('#template').clone();
-newdiv.attr("id", counter);
-newdiv.addClass("div"+css_class).appendTo('#container');
-newdiv.show();
+  var newdiv = $('#template').clone();
+  newdiv.attr("id", counter);
+  newdiv.addClass("div"+css_class).appendTo('#container');
+  newdiv.show();
 // }, 10);
 
     //modify each content div
     $('#container').children('div:not(:first)').each(function(){
-      
+
       $(this).removeClass().addClass("div"+css_class);
       
     });
     if (counter >= 5) {
-       console.log("counter is "+counter);
-       for (i = 1; i <= counter - 4; i++) {
-        console.log("i is "+i);
+     for (i = 1; i <= counter - 4; i++) {
        $('#'+i).removeClass().addClass("divtabhidden");
-      }
-    }
+       var tab = $(".tab_template").clone();
+       tab.removeAttr("id");
+       tab.children("a").text(i);
+       tab.show();
+       tab.appendTo('#tab_container');
+     }
+
+   }
   }); //END ADD click
 
    //remove a search 
- $('#container').on('click', '.tweet_remove', function() {
+   $('#container').on('click', '.tweet_remove', function() {
 
-  //reaffect ID's
-  
-  alert($(this).parent().parent().attr("id"));
-   $(this).killTime('filling');
-   if (counter == 1) {
+     $(this).killTime('filling');
+     if (counter == 1) {
       //empty title
-    $(this).parent().next().html('');
+      $(this).parent().next().html('');
       console.log("first div");
     }
 
     else {
       if (counter == 2)
         $('#container').width("50%") ;
-    
-    $(this).parent().parent().remove();
+
+      $(this).parent().parent().remove();
 //       if (counter < 5) { //no tabs yet
-       counter--;
-      var i = 1;
-      $('#container').children('div:not(:first)').each(function(){
-      $(this).removeClass().addClass("div"+counter);
-      $(this).attr("id", i);
-      i++;
-     
-    });
-// //resize container todo
-//     }
-  }
+ counter--;
+
+  //reaffect ID's so that we can delete any div
+  var i = 1;
+  $('#container').children('div:not(:first)').each(function(){
+    $(this).removeClass().addClass("div"+counter);
+    $(this).attr("id", i);
+    i++;
+
+  });
+}
 });
 
   // MAIN
@@ -80,10 +84,13 @@ newdiv.show();
     first_div.attr("id", "1");
     first_div.appendTo('#container');
     $('#template').hide();
+    $('.tab_template').hide();
+    
+
 //end init container
 
 //
-  $('.tweet').click(function(event) {
+$('.tweet').click(function(event) {
   console.log("hello "+ $(this).html());
 });
 //
@@ -104,7 +111,7 @@ $('#container').on('click', '.search', function() {
   to_search = to_search.replace('#', '%23');
 
   //first call launches timers, others kills before new search
-   if ($(this).attr("name") == 'start')
+  if ($(this).attr("name") == 'start')
     $(this).attr("name", "stop");
   else {
    $(this).killTime('filling');
@@ -127,38 +134,39 @@ $(this).everyTime(15000, 'filling', function() {
     data: 'exp='+ to_search,
     type:       'GET',
     dataType: "json",
-       success: function(data) {
-        console.log("OK");
+    success: function(data) {
+      console.log("OK");
        //appending to the div
-        $(that).parent().next().next().prepend(data);  
-        return data;
+       $(that).parent().next().next().prepend(data);  
+       return data;
 
-      },
-      error : function(data){
-        console.log("error");
-      }
-    });
+     },
+     error : function(data){
+      console.log("error");
+    }
+  });
 }
 
 //DELEGATE
 $("#container").delegate('.tweet', 'click', function(event) {
-    var that = this;
-    $(this).children(".tweet_details").toggle('fast', function() {
+  var that = this;
+  $(this).children(".tweet_details").toggle('fast', function() {
     // Animation complete.
   });
-    console.log("delegate"+$(this).children(".tweet_content").attr("display"));
-  });
+  console.log("delegate"+$(this).children(".tweet_content").attr("display"));
+});
 //
     //slide panel
     var sipPos = 0;
-    $("#paneltab").click(function(e) {
+    $(".tab").click(function(e) {
       e.preventDefault();
-      $("#panel").animate({ left: sipPos }, 800, 'linear', function() {
+
+      $("#tab_container").animate({ left: sipPos }, 800, 'linear', function() {
         if(sipPos == 0) { sipPos = -100; }
         else { sipPos = 0; }
       });
   });//END slide panel
-  $('input[type="submit"]').val('M');
+    $('input[type="submit"]').val('M');
   });//END main
 })(jQuery);
 
